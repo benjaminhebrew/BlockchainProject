eb4df5a66680e9f0b861c01c63995b2a6462ad8b
monster <monster@ddd.com>
2023-05-01 21:55:12 -0700

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import {
+	StyledHome
+} from "../styles/Home.styled";
+
+const Home = () => {
+	return (
+		<StyledHome id="home">
+			
+		</StyledHome>
+	);
+}
+
+export default Home;
---
+++ src/components/styles/Theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+	font: {
+		montserrat: "'Montserrat', sans-serif",
+		cardo: "'Cardo', serif",
+	},
+	color: {
+		dark: "#000",
+		light: "#fff",
+	},
+	backgroundColor: {
+		dark: "#0d0e0e",
+		light: "#f2f2f2",
+	},
+	breakpoints: {
+		lg: "64rem", //1024px
+		md: "58.125rem", //930px
+		sm: "48rem", //768ox
+		xs: "31.25rem", //500px
+	}
+};
