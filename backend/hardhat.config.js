faf1d1780743485ecb6e2289ed1b3b195c62e16d
tangledpuzzle <haroldpatton751@gmail.com>
2023-05-09 16:08:11 -0700

    fix:backend

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,8 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.16",
+  solidity: "0.8.0",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545/",
--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,16 +1,35 @@
 const { ethers } = require("hardhat");
+const path = require("path");
 
 async function main() {
   //STKN
-  console.log("Deploying STKN Contract...");
-  const STKNFactory = await ethers.getContractFactory("STKN");
-  const stkn = await STKNFactory.deploy();
 
+  if (network.name === "hardhat") {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+        "gets automatically created and destroyed every time. Use the Hardhat" +
+        " option '--network localhost'"
+    );
+  }
+
+  const [deployer] = await ethers.getSigners();
+  console.log(
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
+  );
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  console.log("Deploying STKN Contract...");
+  const STKNFactory = await ethers.getContractFactory("ICO_Token_Contract");
+  const stkn = await STKNFactory.deploy();
+  await stkn.deployed();
   console.log("Deployed STKN:", stkn.address);
+  saveFrontendFiles(stkn);
 
   //STKNICO
   console.log("Deploying stknICO Contract...");
-  const StknICOFactory = await ethers.getContractFactory("StknICO");
+  const StknICOFactory = await ethers.getContractFactory("ICO_Contract");
   const stknICO = await StknICOFactory.deploy(
     "0x00f2a05f8327ac26e1994b92dbd4e4813bfa8609",
     stkn.address
@@ -25,3 +44,24 @@ main()
     console.log(error);
     process.exit(1);
   });
+
+function saveFrontendFiles(token) {
+  const fs = require("fs");
+  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    path.join(contractsDir, "contract-address.json"),
+    JSON.stringify({ Token: token.address }, undefined, 2)
+  );
+
+  const TokenArtifact = artifacts.readArtifactSync("ICO_Contract");
+
+  fs.writeFileSync(
+    path.join(contractsDir, "Token.json"),
+    JSON.stringify(TokenArtifact, null, 2)
+  );
+}
